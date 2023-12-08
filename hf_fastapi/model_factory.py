9aa278c2e5ac98736ad5a724c7472773bd66024a
maharshi95 <mgor@cs.umd.edu>
2023-12-08 01:03:37 -0500

    Added new models to model_factory and refactored

--- hf_fastapi/model_factory.py
+++ hf_fastapi/model_factory.py
@@ -2,12 +2,11 @@ import torch
 
 default_configs = {
     "device_map": "auto",
-    "torch_dtype": torch.float16,
-    "max_new_tokens": 20,
+    "torch_dtype": "auto",
     "model_kwargs": {},
 }
 
-PIPELINE_CONFIGS = {
+_llama_pipeline_configs = {
     "llama2-chat-7b": {
         **default_configs,
         "model": "meta-llama/Llama-2-7b-chat-hf",
@@ -20,32 +19,9 @@ PIPELINE_CONFIGS = {
         **default_configs,
         "model": "meta-llama/Llama-2-70b-chat-hf",
     },
-    "opt-iml-max-30b": {
-        **default_configs,
-        "model": "facebook/opt-iml-max-30b",
-        "torch_dtype": "auto",
-        "model_kwargs": {"load_in_8bit": True},
-    },
-    "t5-xxl": {
-        **default_configs,
-        "model": "google/t5-v1_1-xxl",
-        "torch_dtype": torch.bfloat16,
-    },
-    "T0pp-11b": {
-        **default_configs,
-        "model": "bigscience/T0pp",
-        "torch_dtype": torch.bfloat16,
-    },
-    "T0p-11b": {
-        **default_configs,
-        "model": "bigscience/T0p",
-        "torch_dtype": torch.bfloat16,
-    },
-    "T0-11b": {
-        **default_configs,
-        "model": "bigscience/T0",
-        "torch_dtype": torch.bfloat16,
-    },
+}
+
+_mistral_pipeline_configs = {
     "mistral-7b": {
         **default_configs,
         "model": "mistralai/Mistral-7B-v0.1",
@@ -65,5 +41,68 @@ PIPELINE_CONFIGS = {
         **default_configs,
         "model": "HuggingFaceH4/zephyr-7b-beta",
         "torch_dtype": torch.float32,
+    },
+}
+
+_bigscience_pipeline_configs = {
+    "T0pp-11b": {
+        **default_configs,
+        "model": "bigscience/T0pp",
+        "torch_dtype": torch.bfloat16,
+    },
+    "T0p-11b": {
+        **default_configs,
+        "model": "bigscience/T0p",
+        "torch_dtype": torch.bfloat16,
+    },
+    "T0-11b": {
+        **default_configs,
+        "model": "bigscience/T0",
+        "torch_dtype": torch.bfloat16,
+    },
+}
+
+_falcon_pipeline_configs = {
+    "falcon-rw-1b": {
+        **default_configs,
+        "model": "tiiuae/falcon-rw-1b",
+        "torch_dtype": torch.bfloat16,
+        "trust_remote_code": True,
+    },
+    "falcon-7b": {
+        **default_configs,
+        "model": "tiiuae/falcon-7b",
+        "torch_dtype": torch.bfloat16,
+        "trust_remote_code": True,
+    },
+    "falcon-7b-inst": {
+        **default_configs,
+        "model": "tiiuae/falcon-7b-instruct",
+        "torch_dtype": torch.bfloat16,
+        "trust_remote_code": True,
+    },
+    "falcon-40b": {
+        **default_configs,
+        "model": "tiiuae/falcon-40b",
+        "torch_dtype": torch.bfloat16,
+        "trust_remote_code": True,
+    },
+}
+
+PIPELINE_CONFIGS = {
+    **_llama_pipeline_configs,
+    **_mistral_pipeline_configs,
+    **_bigscience_pipeline_configs,
+    **_falcon_pipeline_configs,
+    "opt-iml-max-30b": {
+        **default_configs,
+        "model": "facebook/opt-iml-max-30b",
+        "torch_dtype": "auto",
+        "model_kwargs": {"load_in_8bit": True},
+    },
+    "t5-xxl": {
+        **default_configs,
+        "model": "google/t5-v1_1-xxl",
+        "torch_dtype": torch.bfloat16,
     },
 }
