a1d3dc8b29ad6749d0f7a8f9a4c49386ddfe6b10
maharshi95 <mgor@cs.umd.edu>
2023-12-08 01:04:38 -0500

    Added support for falcon models

--- hf_fastapi/models.py
+++ hf_fastapi/models.py
@@ -1,4 +1,4 @@
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer
 from transformers.pipelines import Pipeline, TextGenerationPipeline
 
 
@@ -6,6 +6,8 @@ def get_custom_generate_kwargs(llm_pipeline: Pipeline):
     kwargs = {}
     if "mistral" in llm_pipeline.model.name_or_path:
         kwargs["pad_token_id"] = llm_pipeline.tokenizer.eos_token_id
+    if "falcon-" in llm_pipeline.model.name_or_path:
+        kwargs["eos_token_id"] = llm_pipeline.tokenizer.eos_token_id
     if isinstance(llm_pipeline, TextGenerationPipeline):
         kwargs["return_full_text"] = False
     return kwargs
@@ -14,12 +16,18 @@ def get_custom_generate_kwargs(llm_pipeline: Pipeline):
 class HFModel:
     def __init__(self, pipeline_config: dict) -> None:
         self.model_name = pipeline_config["model"]
-        self.pipe = pipeline(**pipeline_config)
+        tokenizer_name = pipeline_config.get("tokenizer", self.model_name)
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+        self.pipe = pipeline(**pipeline_config, tokenizer=tokenizer)
         self.generate_kwargs = get_custom_generate_kwargs(self.pipe)
 
     @property
     def device(self):
         return self.pipe.device
+
+    @property
+    def torch_dtype(self):
+        return self.pipe.model.dtype
 
     def generate(self, prompt: str, **kwargs) -> str:
         output = self.pipe(prompt, **self.generate_kwargs, **kwargs)
